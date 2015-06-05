a467e4de5d145e2dfd0fb96d4eadf467ef085964
Benjamin Réthoré <ben@MacBeche.local>
2015-06-05 17:41:25 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+    'use strict';
+    
+    var path = require('path');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        premailer: {
+            main: {
+                files: {
+                    './index.inline.html': ['./index.html'],
+                }
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },             
+                files: [{
+                    expand: true,
+                    cwd: './scss/',
+                    src: ['**/*.scss'],
+                    dest: './css/',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        watch: {
+            css: {
+                files: ['./scss/**/*.scss'],
+                tasks: ['sass', 'premailer:main'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-premailer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', [
+        'sass',
+        'premailer:main',
+        'watch'
+    ]);
+
+    grunt.registerTask('default', 'build');
+};
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Hey, i'm Grido, former bounty hunter, now responsive email grid system
+
+
+
+### Ressources :
+
+- Inline your styles with [Campaign monitor inliner](http://inliner.cm/).
+- Test your email with [Litmus](https://litmus.com/).
+- Hang with me at [the Cantina](http://www.starwars.com/databank/mos-eisley-cantina).
